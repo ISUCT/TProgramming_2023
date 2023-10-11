3435d7aa31750343869f80a6d7a029b262357f6c
Demonage <tortoisegames431@gmail.com>
2023-10-11 17:01:29 +0300

    Added tests for getters and setters

--- rpgsaga/saga/tests/pig.spec.ts
+++ rpgsaga/saga/tests/pig.spec.ts
@@ -11,6 +11,23 @@ describe('Testing pig constructor', () => {
     });
 });
 
+describe ('Testing pig setters and getters', () => {
+    it('Normal mass', () => {
+        const pig = new Pig(50, "ID823");
+        expect(pig.mass).toEqual(50);
+    })
+    it('Normal mass (float)', () => {
+        const pig = new Pig(50.283, "ID823");
+        expect(pig.mass).toEqual(50.283);
+    })
+    it('Less than normal mass', () => {
+        expect(() => new Pig(25, "ID823")).toThrow(Error("Pig's mass should be 50 kg or bigger."));
+    })
+    it('Negative mass', () => {
+        expect(() => new Pig(-5, "ID823")).toThrow(Error("Pig's mass should be 50 kg or bigger."));
+    })
+});
+
 describe('Testing pig methods', () => {
     it('Should give right amount of сало', () => {
         const pig = new Pig(100, "ADB69");
