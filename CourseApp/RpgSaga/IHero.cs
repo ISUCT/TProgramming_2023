6b97f7bedf037e87bc2715c83dbccbcce60e151b
Fuflick <stepan_belov_2016@mail.ru>
2023-10-24 09:07:27 +0300

    Try to fix conflict

---
+++ CourseApp/RpgSaga/IHero.cs
@@ -0,0 +1,18 @@
+﻿namespace CourseApp.RpgSaga;
+
+using System;
+
+public interface IHero
+{
+    public string Name;
+
+    public int Strength;
+
+    public int Health;
+
+    public Random Rand = new Random();
+
+    public int SetValue();
+
+    public void Atack(IHero person);
+}
---
+++ CourseApp/RpgSaga/Warior.cs
@@ -0,0 +1,15 @@
+﻿namespace CourseApp.RpgSaga;
+
+public class Warior : IHero
+{
+    public int SetValue()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Atack(IHero person)
+    {
+    }
+    
+    
+}
