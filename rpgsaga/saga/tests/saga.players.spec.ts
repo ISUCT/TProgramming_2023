1cd0b7dacc8b3405c086e796b4dd0321dd317345
fagirton <fagirton2015@gmail.com>
2023-12-08 23:38:33 +0300

    Oh no! Tests???

---
+++ rpgsaga/saga/tests/saga.players.spec.ts
@@ -0,0 +1,31 @@
+import { Player } from '../src/saga/player';
+import { PlayerGenerator } from '../src/saga/playerGenerator';
+
+describe('Testing creating players', () => {
+  it('should return as much players as we ordered', () => {
+    const gen = new PlayerGenerator();
+    const players = gen.createPlayers(4, 4, 4);
+    expect(players).toBeDefined();
+    const nameList = {
+      knight: 0,
+      archer: 0,
+      mage: 0,
+    };
+    for (let i = 0; i < players.length; i++) {
+      switch (players[i].constructor.name) {
+        case 'Knight':
+          nameList.knight += 1;
+          break;
+        case 'Archer':
+          nameList.archer += 1;
+          break;
+        case 'Mage':
+          nameList.mage += 1;
+          break;
+      }
+    }
+    expect(nameList.knight).toEqual(4);
+    expect(nameList.archer).toEqual(4);
+    expect(nameList.mage).toEqual(4);
+  });
+});
