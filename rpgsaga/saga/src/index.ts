fe467eee4e9ccbbda7cd47caca513c6749418f94
alkkas <alliscakess@gmail.com>
2024-01-06 16:53:58 +0300

    add saga

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,16 +1,5 @@
-import { Phone } from './phone';
+import { Game } from './game';
 
-const first = new Phone('+7900-000 000 (123)', 1990, 'Телефон 1');
-first.year = 1998;
+const game = new Game(8);
 
-first.year = -1998;
-first.call('12345');
-first.endCall();
-
-const second = new Phone('+799900000', -5);
-// second.name = 'Телефон 2';
-console.log(second.year);
-second.call('12345');
-second.endCall();
-
-console.log(first, second, Phone.phoneCount);
+game.start();
