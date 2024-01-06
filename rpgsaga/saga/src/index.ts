d757aa40367531aed0457335911b0f1942f75f53
Demonage <tortoisegames431@gmail.com>
2024-01-06 17:58:08 +0300

    Now user can choose the amount of players (typing in the console). Also changed a launch.json a bit and downloaded a promptSync package in order to get it to work.

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,4 +1,14 @@
+import * as promptSync from 'prompt-sync';
+
 import { Game } from './game';
 
-const game = new Game(3);
-game.start();
+const prompt = promptSync();
+
+const quantityOfPlayers: number = parseInt(prompt('Enter the amount of players (bigger than 1): '));
+
+if (quantityOfPlayers > 1) {
+  const game = new Game(quantityOfPlayers);
+  game.start();
+} else {
+  console.log('Incorrect amount of players, try again!');
+}
