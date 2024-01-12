4b80d600ad1822f912ad44fe85e6331dde7936ec
EgoriusTheGreat2 <117865797+EgoriusTheGreat2@users.noreply.github.com>
2024-01-12 17:50:00 +0300

    Update index.ts

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,5 +1,8 @@
 import { Cat } from './cat';
+import {Game} from "./Game";
 
 const Cat1 = new Cat (8, 'Кеша', 'Шотландский вислоухий', 7);
-
 Cat1.getInfo()
+
+const game = new Game(8);
+game.start();
