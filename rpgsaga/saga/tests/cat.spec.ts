b24426d1ea1bffcaa30d5e29f64400f68adc3062
EgoriusTheGreat2 <117865797+EgoriusTheGreat2@users.noreply.github.com>
2024-01-12 17:44:25 +0300

    Update cat.spec.ts

--- rpgsaga/saga/tests/cat.spec.ts
+++ rpgsaga/saga/tests/cat.spec.ts
@@ -1,34 +1,32 @@
 import { Cat } from '../src/cat';
 
-describe('Testing cat constructor', () => {
-  it('Cat should be created', () => {
-    const cat1 = new Cat(7, 'Кеша', 'Шотландский вислоухий', 5);
-    expect(cat1.Age).toEqual(7);
-    expect(cat1.Name).toEqual('Кеша');
-    expect(cat1.Breed).toEqual('Шотландский вислоухий');
-    expect(cat1.Weight).toEqual(5);
-  });
-});
-
-describe('Testing cat methods', () => {
-  it('Cat new age value', () => {
-    const cat1 = new Cat(7, 'Кеша', 'Шотландский вислоухий', 5);
-    cat1.Age = 7;
-    expect(cat1.Age).toEqual(7);
-  });
-  it('Cat new name value', () => {
-    const cat1 = new Cat(7, 'Кеша', 'Шотландский вислоухий', 5);
-    cat1.Name = 'Кеша';
-    expect(cat1.Name).toEqual('Кеша');
-  });
-  it('Cat new weight', () => {
-    const cat1 = new Cat(7, 'Кеша', 'Шотландский вислоухий', 5);
-    cat1.Weight = 5;
-    expect(cat1.Weight).toEqual(5);
-  });
-  it('Cat new breed', () => {
-    const cat1 = new Cat(7, 'Кеша', 'Шотландский вислоухий', 5);
-    cat1.Breed = 'Шотландский вислоухий';
-    expect(cat1.Breed).toEqual('Шотландский вислоухий');
-  });
+describe('Testing Cat constructor', () => {
+    it('Cat should be created', () => {
+        const first = new Cat(8, "Кеша", "Шотландский вислоухий", 7);
+        expect(first.age).toEqual(8);
+        expect(first.name).toEqual("Кеша");
+        expect(first.breed).toEqual("Шотландский вислоухий");
+        expect(first.weight).toEqual(7);
+    });
+    it('Cat with empty name', () => {
+        const first = new Cat(8, "", "Шотландский вислоухий", 7 );
+        expect(first.age).toEqual(8);
+        expect(first.name).toBeUndefined();
+        expect(first.breed).toEqual("Шотландский вислоухий");
+        expect(first.weight).toEqual(7);
+    });
+        it('Cat age lower than bound', () => {
+        const first = new Cat(0, "", "Шотландский вислоухий", 7 );
+        expect(first.age).toEqual(1);
+        expect(first.name).toBeUndefined();
+        expect(first.breed).toEqual("Шотландский вислоухий");
+        expect(first.weight).toEqual(7);
+    });
+    it('Cat age higher than bound', () => {
+        const first = new Cat(65, "", "Шотландский вислоухий", 7 );
+        expect(first.age).toEqual(29);
+        expect(first.name).toBeUndefined();
+        expect(first.breed).toEqual("Шотландский вислоухий");
+        expect(first.weight).toEqual(7);
+    });
 });
