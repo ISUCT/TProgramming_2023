8bb529f2fd6c0448e35d2570d5e918dbe2bcb7c3
EgoriusTheGreat2 <117865797+EgoriusTheGreat2@users.noreply.github.com>
2024-01-12 17:42:54 +0300

    Update cat.ts

--- rpgsaga/saga/src/cat.ts
+++ rpgsaga/saga/src/cat.ts
@@ -1,44 +1,20 @@
 export class Cat {
-  private theAge: number;
-  private theName: string;
-  private theBreed: string;
-  private theWeight: number;
+  Age: number;
+  Name: string;
+  Breed: string;
+  Weight: number;
 
   constructor(age: number, name: string, breed: string, weight: number) {
-    this.theAge = age;
-    this.theName = name;
-    this.theBreed = breed;
-    this.theWeight = weight;
-  }
-   set Age(age: number){
-    this.theAge = age;
-  }
-
-  get Age(): number {
-    return this.theAge;
-  }
-
- set Name(name: string){
-    this.theName = name;
-  }
-
-  get Name(): string {
-    return this.theName;
-  }
-
- set Breed(breed: string){
-    this.theBreed = breed;
-  }
-  
-  get Breed(): string {
-    return this.theBreed;
-  }
-  set Weight(weight: number){
-    this.theWeight = weight;
-  }
-
-  get Weight(): number{
-    return this.theWeight;
+    this.Age = age;
+    this.Name = name;
+    this.Breed = breed;
+    this.Weight = weight;
+    if (age <= 0 || age > 30){
+            throw new Error("Invalid age");
+        }
+        if (weight <= 0 || weight > 30){
+            throw new Error("invalid weight")
+        }
   }
  getInfo(): string {
       return `Имя: ${this.theName}; Порода: ${this.theBreed}; Возраст: ${this.theAge}; Вес:${this.theWeight}.`
